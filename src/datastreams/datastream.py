ecf35baf8f423099e12c2956d7a11baf02ef8573
Evan-Kim2028 <ekcopersonal@gmail.com>
2023-01-20 15:25:19 -0500

    add DataStream cross-subgraph query functionality

--- src/datastreams/datastream.py
+++ src/datastreams/datastream.py
@@ -1,39 +1,88 @@
 from dataclasses import dataclass
 from datastreams.streamer import Streamer
-
+from functools import reduce
 from subgrounds.subgrounds import Subgrounds
+from pandas import DataFrame
 
 @dataclass
 class DataStream:
     """
-    ** CURRENTLY BEING BUILT. NOT USABLE YET **
-    Use DataStream to interface with the Streamer class. Future direction of the class is undecided. 
-    The only benefit is abstracting away the execution layer from Streamer class. 
+    DataStream is used to manage multiple Streamer objects. In other words, you can manage multiple Subgraph queries
+    with DataStream.
 
-    Tentatively DataStream should store execution functionality whereas Streamer will store
-    the subgraph interface functionality. 
-
-    Additionally, Streamer should only interact with a single endpoint whereas DataStream can interact 
-    multiple endpoints.
+    #. Insert a list of endpoints, each one represents a Subgraph endpoint.
     """
+    endpointList: list[str] = None
+    streamerDict: dict = None
 
-    # Subgrounds is unique for every DataStream?
-    def getDataStream(self, endpoint: str, query_size = 5):
+    def __post_init__(self):
+        # generate Streamer names
+        # generate Streamer objects
+        # combine into dictionary
+        self.makeStreamerDict()
+
+    def makeStreamerDict(self):
         """
-        :param str endpoint: graphql endpoint
-        :param int query_size: number of queries to run. Default is 5.
-
-
-        getDataStream is a firehose that returns all data from a subgraph endpoint.
-        Should query_size, a graphQl parameter be contained in this function? Or moved somewhere else
+        generate streamerDict from endpointList
         """
-        # create new Subgrounds object
-        sub = Subgrounds()
-        # load subgraph data into Subgrounds
-        subgraph = sub.load_subgraph(endpoint)
+        self.streamerDict: dict = {}
 
-        # load Subgrounds data into Streamer
-        streamer = Streamer(sub, endpoint, subgraph)
+        for endpoint in self.endpointList:
+            # generate the name from the endpoint as the string seperated by the right most /
+            name = endpoint.split('/')[-1]
+            # create new Streamer object
+            streamer = Streamer(endpoint)
+            # add to streamerDict dict
+            self.streamerDict[name] = streamer
 
-        # run Streamer. By default runStreamer is a firehose.
-        streamer.runStreamer(query_size)
+    def getCommonQueryKeys(self, streamerList: dict = None) -> list[str]:
+        """
+        getCommonQueryKeys returns a list of query keys that are common to all Streamer objects.
+
+        :param list[Streamer] streamerList: list of Streamer objects. Default is None, which will use all Streamer objects in streamerDict
+        """
+        if streamerList:
+            common_keys = list(reduce(set.intersection, [set(ds.queryDict.keys()) for ds in streamerList.values()]))
+        else:
+            common_keys = list(reduce(set.intersection, [set(ds.queryDict.keys()) for ds in self.streamerDict.values()]))
+
+        return common_keys
+
+
+    def querySubgraphs(self, streamer_dict: dict, query_field: str, query_size: int = 7) -> list[DataFrame]:
+        """
+        query all Subgraphs in a streamer dictionary for a specific query field
+
+        :param dict streamer_dict: dictionary of Streamer objects
+        :param str query_field: query field to run
+        :param int query_size: number of queries to run. Default is 7.
+        """
+        data = []
+        #run token query on streamer objects
+        for ds in streamer_dict.values():
+            print(f'querying {ds.endpoint} for {query_field}...')
+            # token query
+            tokens_df = ds.runQuery(ds.queryDict[query_field], query_size=query_size)
+            data.append(tokens_df)
+        return data
+
+
+
+    # # Subgrounds is unique for every DataStream?
+    # def getDataStream(self, endpoint: str, query_size = 5):
+    #     """
+    #     :param str endpoint: graphql endpoint
+    #     :param int query_size: number of queries to run. Default is 5.
+
+
+    #     getDataStream is a firehose that returns all data from a subgraph endpoint.
+    #     Should query_size, a graphQl parameter be contained in this function? Or moved somewhere else
+    #     """
+    #     # create new Subgrounds object
+    #     sub = Subgrounds()
+    #     # load subgraph data into Subgrounds
+    #     subgraph = sub.load_subgraph(endpoint)
+
+    #     # load Subgrounds data into Streamer
+    #     streamer = Streamer(sub, endpoint, subgraph)
+
--- tests/test_datastream.py
+++ tests/test_datastream.py
@@ -1,15 +1,40 @@
 from datastreams.datastream import DataStream
 
 
-ds = DataStream()
 
-endpoint_ids = [
-                'https://api.thegraph.com/subgraphs/name/cowprotocol/cow',
-                'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-gc'
-                ]
+endpointList = [
+    'https://api.thegraph.com/subgraphs/name/messari/sushiswap-ethereum', 
+    'https://api.thegraph.com/subgraphs/name/messari/balancer-v2-ethereum',
+    'https://api.thegraph.com/subgraphs/name/messari/curve-finance-ethereum',
+    'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2', # https://thegraph.com/hosted-service/subgraph/uniswap/uniswap-v2 # NON-MESSARi SUBGRAPH
+    'https://api.thegraph.com/subgraphs/name/messari/uniswap-v3-ethereum',
+    'https://api.thegraph.com/subgraphs/name/messari/saddle-finance-ethereum'
+    ]
 
 
-# test getDataStream() functionality in a loop
-for endpoint in endpoint_ids:
-    ds.getDataStream(endpoint)
 
+ds = DataStream(endpointList)
+
+# endpoints loaded into DataStream
+print(f' the endpoints are: {ds.streamerDict.keys()}')
+
+# check the keys for a single Streamer "sushiswap-ethereum"
+# print(ds.streamerDict['sushiswap-ethereum'].queryDict.keys())
+
+common_keys = ds.getCommonQueryKeys()
+print(f'common_keys: {common_keys}') # the common_keys are very small because uniswap-v2 is not a messari standardized subgraph.
+
+# make a new streamerDict excluding uniswap-v2
+new_dict = ds.streamerDict
+new_dict.pop('uniswap-v2')
+
+
+new_common_keys = ds.getCommonQueryKeys(streamerList=new_dict)
+print(f'new_common_keys: {new_common_keys}') # we now have a much larger list of common keys after dropping uniswap-v2
+
+tokens_dfs = ds.querySubgraphs(new_dict, 'tokens', query_size=7)
+print(tokens_dfs)
+
+
+
+print('FINISHED')
